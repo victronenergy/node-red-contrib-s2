8f4730417341ae65cf693d5fe0d940fcc5296786
Chris Oloff <chris@uber5.com>
2025-09-08 11:57:27 +0200

    add node-red node in typescript, with build via 'npm run build', wip

---
+++ src/my-node.ts
@@ -0,0 +1,15 @@
+import { NodeAPI } from 'node-red';
+
+module.exports = function(RED: NodeAPI) {
+  function MyNode(config: any) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+
+    node.on('input', function(msg: any) {
+      msg.payload = (msg.payload || '') + ' processed by MyNode';
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType('my-node', MyNode);
+}
